adf6c9bb32ef0a9daf679ec57529372ce7b81fb7
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2025-06-01 15:36:55 -0400

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { LandingPage } from "@/components/landing-page";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
+  return 
+
+LandingPage
